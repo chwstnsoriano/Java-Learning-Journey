cfd8a00dfceed7cf065a4f4d29c2d3e46a368b4d
chwstnsoriano <162024320+chwstnsoriano@users.noreply.github.com>
2025-12-03 14:01:01 +0800

    Add files via upload

---
+++ 01-LearningPrint/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args){
+
+        //This will be my first step to learn java
+
+        System.out.println("Hello fucking world");
+        System.out.println("Here we go again");
+        System.out.println("Learning programming seriously");
+
+    }
+
+}
